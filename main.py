d49d239a43baa7624e0c4e5dbc6a9e1879c91bed
lexman02 <10473497+lexman02@users.noreply.github.com>
2022-10-31 23:58:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,6 @@ host='127.0.0.1'
 port='5432'
 database='dvdrental'
 
-@app.route('/')
-def index():
-    cursor, connection = util.connect_to_db(username,password,host,port,database)
-    record = util.run_and_fetch_sql(cursor, "SELECT * from customer;")
-    if record == -1:
-        print('Something is wrong with the SQL command')
-    else:
-        col_names = [desc[0] for desc in cursor.description]
-        log = record[:5]
-    util.disconnect_from_db(connection,cursor)
-    return render_template('index.html', sql_table = log, table_title=col_names)
-
 @app.route('/api/update_basket_a')
 def update_basket_a():
     cursor, connection = util.connect_to_db(username,password,host,port,database)
